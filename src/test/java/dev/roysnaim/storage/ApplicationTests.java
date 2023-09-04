9f255699100ddefb356e5be9e0932b3fb37e8121
Roy Naim <roysnaim@gmail.com>
2023-09-04 18:33:30 +0300

    initial commit

---
+++ src/test/java/dev/roysnaim/storage/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.roysnaim.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
